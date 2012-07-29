7577b00115f4bfbf015907efd1352f772e7b9914
Rob Yurkowski <rob@yurkowski.net>
2012-07-29 01:42:44 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineryUi::Application.config.secret_token = '12a9f4176c1cbd4b8d69f64a2f74a97a11e6f0a30ece87e4ebf8bdf1245a55616c4c1deb0782133800d691d0709a6a577c9a45a874fec59c133e6ef5d41acf14'
